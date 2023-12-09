279646d27ac2dcc4b2021ec56fef122be17adf8a
PLThabangR <rakgoropthabang@gmail.com>
2023-12-09 23:57:32 +0200

    first commit

---
+++ bashsyntax.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+echo "Learning indentation"
+ x=0
+echo
+if [ $x -eq 0 ]
+then
+  echo "In the if block"
+  echo "The value is Zero"
+else
+  echo "In the else block"
+  echo "The value is not Zero"
+fi
+echo
+echo "This statement is out of the else if block"
---
+++ test.py
@@ -0,0 +1,13 @@
+print("Learning python")
+x=0
+print()
+
+if x == 0:
+    print("In the if Block")
+    print("Value of x is 0")
+else:
+     print("In the else Block")
+     print("Value of x is not 0")
+
+print()
+print("This statement is not in the ifElse block")
