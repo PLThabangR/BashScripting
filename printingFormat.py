4318d877d7919bee2eaf9c02d6022655b0a5c7e5
PLThabangR <rakgoropthabang@gmail.com>
2023-12-10 00:14:30 +0200

    printing and format in python

---
+++ printingFormat.py
@@ -0,0 +1,7 @@
+name="sars_cov_2"
+disease="Covid19"
+
+print("The anme of the virus is",name)
+print("The name of virus is ",name,"associated disease is",disease)
+print("The name of virus is {} associated disease is {}".format(name,disease))
+print("The name of the virus "+name+"associated disease is"+disease)
