b32588dd882142782c8cd73723cae49008fbc5cd
PLThabangR <rakgoropthabang@gmail.com>
2023-12-10 02:01:52 +0200

    test vaccines using python conditional logic

---
+++ conditions.py
@@ -0,0 +1,22 @@
+x= 20
+if(x<10):
+    print("Inside if block")
+    print("X is less than 20")
+print("Rest of the code")
+
+if(x<10):
+    print("Inside if block")
+    print("X is less than 20")
+else:
+    print("X is greater than 20")
+print("Rest of the code")
+
+y=9
+if y>10:
+    print("Inside if block")
+    print("Y greater than 10")
+elif y==10 :
+    print("Y is equal to 10")
+else:
+    print("Y is less than 10")
+print("Rest of the code")
